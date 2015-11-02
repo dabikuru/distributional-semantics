2a215a4663954716fe16655d8ba50c79f8758c16
Davide Cultrera <davidec93@gmail.com>
2015-11-02 09:48:53 +0000

    Read files in; matrix almost ready

--- dsm/build_vectors.py
+++ dsm/build_vectors.py
@@ -1,5 +1,7 @@
 from itertools import islice
 from collections import Counter
+import pickle as p
+import csv
 
 
 class Window:
@@ -13,11 +15,11 @@ class Window:
         :param n: window size (on one side)
         """
         self.seq = iter(seq)
-        self.n = n*2 + 1
+        self.n = n * 2 + 1
 
         self.result = None
         self.word = None
-        self.neighbours = self.result[:n//2] + self.result[(n//2)+1:]
+        self.neighbours = self.result[:n // 2] + self.result[(n // 2) + 1:]
 
     def __iter__(self):
         return self
@@ -25,13 +27,13 @@ class Window:
     def __next__(self):
         if self.result is None:
             self.result = tuple(islice(self.seq, self.n))
-            self.word = self.result[self.n//2]
-            return self.result
+            self.word = self.result[self.n // 2]
+            return self.word, self.neighbours
 
         for elem in self.seq:
             self.result = self.result[1:] + (elem,)
-            self.word = self.result[self.n//2]
-            return self.result
+            self.word = self.result[self.n // 2]
+            return self.word, self.neighbours
 
 
 class Matrix:
@@ -45,8 +47,9 @@ class Matrix:
         """
 
         self.m = dict()
-        self.candiates = candidates
+        self.candidates = candidates
         self.features = features
+        self.corpus = corpus
 
         for c in candidates:
             # Counter defaults to 0 for missing keys – no need to instantiate all of them
@@ -60,13 +63,56 @@ class Matrix:
             return self.m[item[0]][item[1]]
 
     def similarity(self, w1, w2):
-        # return self.m[w1] * self.m[w2]
+        return  # cosine similarity of two vectors
 
     def populate(self):
         """
         Iterate over corpus with a sliding window:
         when the central word is in candidates, add its neighbours to the counter.
-        :return:
         """
-        # return self.corpus
 
+        window = Window(self.corpus, 3)
+        for (word, neighbours) in window:
+            if word in self.candidates:
+                self.m[word].update([n for n in neighbours if (n in self.features)])
+
+
+def read_candidates(f_candidates):
+    """
+    Retrieves target words from a file
+    :param f_candidates: file handle
+    :return: set of target words
+    """
+    reader = csv.reader(f_candidates)
+    candidates = set()
+    for (w1, w2, _) in reader:
+        candidates.add(w1)
+        candidates.add(w2)
+    return candidates
+
+
+def read_features(f_features):
+    """
+    Retrieves feature words from a file
+    :param f_features: file handle
+    :return: set of features
+    """
+    features = set()
+    for f in f_features:
+        features.add(f.split()[0])
+    return features
+
+
+def main():
+    with open('../res/wikicorpus.list', 'r') as f_corpus:
+        with open('../res/frequent_words', 'r') as f_features:
+            features = read_features(f_features)
+        with open('../res/wordsim353/combined.csv', 'r') as f_candidates:
+            candidates = read_candidates(f_candidates)
+
+        model = Matrix(candidates, features, f_corpus)
+        model.populate()
+        with open('../tmp/model.p', 'wb') as fout:
+            p.dump(model, fout)
+
+# TODO: neighbours returns None
